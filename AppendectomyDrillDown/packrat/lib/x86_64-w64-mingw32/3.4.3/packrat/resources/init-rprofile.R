c4791810b92b0ea7c500ed1b5e0ade545dc6e508
Laura Brennan <lbrennan@stratadecision.com>
2018-06-11 20:55:27 -0500

    Revert "Revert "more misc updates""
    This reverts commit 952022b4ded1fca4642d2b4bedd2032a422e9a9e.

--- AppendectomyDrillDown/packrat/lib/x86_64-w64-mingw32/3.4.3/packrat/resources/init-rprofile.R
+++ AppendectomyDrillDown/packrat/lib/x86_64-w64-mingw32/3.4.3/packrat/resources/init-rprofile.R
@@ -1,3 +1,7 @@
+<<<<<<< Updated upstream
 #### -- Packrat Autoloader (version 0.4.9-3) -- ####
+=======
+#### -- Packrat Autoloader (version 0.4.9-2) -- ####
+>>>>>>> Stashed changes
 source("packrat/init.R")
 #### -- End Packrat Autoloader -- ####
